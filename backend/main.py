0ffdff0a1948073d8ddc50b06add3aa4f6d85700
varshaa-7 <awasthivarsha0710@gmail.com>
2026-03-24 11:32:02 +0530

    fix: fixed cors error

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app = FastAPI(title="GenAI Stack API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://no-code-intelligent-workflow-yv2t.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
